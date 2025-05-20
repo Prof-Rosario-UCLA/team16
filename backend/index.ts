8eb53d46dc9e279f14656db668997a337b86ab7e
ghtjason <jasonshuhezhang@gmail.com>
2025-05-19 17:43:42 -0700

    set up typescript + hot reloading with npm run dev

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,16 @@
-const express = require("express");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
 const app = express();
-const http = require("http");
 const server = http.createServer(app);
-const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get("/api/ping", (req, res) => {
+app.get("/api/ping", (req: any, res: any) => {
   res.send("pong");
 });
 
-io.on("connection", (socket) => {
+io.on("connection", (socket: any) => {
   console.log("a user connected");
 });
 
