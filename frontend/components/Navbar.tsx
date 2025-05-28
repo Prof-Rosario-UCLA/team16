ef4a62dab748bbaa7acf2316fc5d233074544664
Andrew Wang <andrewang2004@gmail.com>
2025-05-28 16:42:13 -0700

    forgot to push these components

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import Link from "next/link";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+export default function Navbar() {
+
+  const router = useRouter();
+
+  async function handleLogout() {
+    try {
+      const res = await axios.delete("http://localhost:3001/api/login/session", {
+        withCredentials: true,
+      });
+      if (res.status === 200) {
+        router.push("/login");
+      } 
+      else {
+        console.error("Logout failed", res.statusText);
+      }
+    } catch (err) {
+      console.error("Logout error", err);
+    }
+  }
+
+  return (
+    <div className="absolute inset-0 z-50 h-16 px-10 py-10 flex items-center justify-between">
+      <Link href="/" className="font-bold hover:underline" style={{ color: 'black' }}>doodly</Link>
+      <nav className="space-x-4 flex flex-row items-center">
+        <Link href="/profile" className="hover:underline">Profile</Link>
+        <button
+          onClick={handleLogout}
+          className="hover:underline cursor-pointer nes-btn is-normal"
+          type="button"
+          aria-label="Logout"
+          
+        >
+          Logout
+        </button>
+      </nav>
+    </div>
+  );
+}
