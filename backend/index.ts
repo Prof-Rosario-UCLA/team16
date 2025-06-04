331a5274a6c369373fdecd20f070502fce37dc17
ghtjason <jasonshuhezhang@gmail.com>
2025-06-03 19:22:00 -0700

    added default 200 ok at /, changed imagepullpolicy to latest to always pull most recent image, BACKEND WORKS AT https://team16.cs144.org/ YIPPEE

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
@@ -16,10 +16,11 @@ import cookieParser from "cookie-parser";
 import * as cookie from "cookie";
 import jwt from "jsonwebtoken";
 
-
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "http://localhost:3000", credentials: true } });
+const io = new Server(server, {
+  cors: { origin: "http://localhost:3000", credentials: true },
+});
 
 const corsOptions = {
   origin: "http://localhost:3000", // change on deployment
@@ -29,13 +30,18 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser(process.env.COOKIE_SECRET || 'default'));
+app.use(cookieParser(process.env.COOKIE_SECRET || "default"));
 
 app.use((err: any, req: any, res: any, next: any) => {
-  res.status(err.status || 500).json({ message: err.message || 'Internal server error' });
+  res
+    .status(err.status || 500)
+    .json({ message: err.message || "Internal server error" });
 });
 
 app.use("/api/login", loginRoutes);
+app.get("/", (_, res) => {
+  res.status(200).send("ok");
+});
 
 app.use(verifyToken); // apply auth middleware to all routes below this line
 
@@ -63,7 +69,10 @@ io.use((socket, next) => {
     }
   }
 
-  const cookies = cookieParser.signedCookies(filteredCookies, process.env.COOKIE_SECRET || 'default');
+  const cookies = cookieParser.signedCookies(
+    filteredCookies,
+    process.env.COOKIE_SECRET || "default"
+  );
   const token = cookies.token;
   if (!token) {
     return next(new Error("Token not found or invalid"));
@@ -71,15 +80,18 @@ io.use((socket, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-    if (typeof decoded === "object" && decoded !== null && "username" in decoded) {
+    if (
+      typeof decoded === "object" &&
+      decoded !== null &&
+      "username" in decoded
+    ) {
       socket.data.user = (decoded as jwt.JwtPayload).username;
       console.log("Authenticated user:", socket.data.user);
       next();
     } else {
       return next(new Error("Invalid token payload: username missing"));
     }
-  }
-  catch (err) {
+  } catch (err) {
     console.error("Token verification failed:", err);
     next(new Error("Unauthorized: Invalid token"));
   }
@@ -88,14 +100,14 @@ io.use((socket, next) => {
 io.on("connection", (socket) => setupGameSocket(io, socket));
 
 // startup
-const main = async() => {
+const main = async () => {
   await connectToMongoDB();
 
   const PORT = process.env.PORT;
   server.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
   });
-}
+};
 
 main().catch((err) => {
   console.error("Failed to start server:", err);
