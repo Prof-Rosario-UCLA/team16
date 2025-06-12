2e487e2a1bca6471c9c3c778568a6998cf43f29f
ghtjason <jasonshuhezhang@gmail.com>
2025-06-11 17:57:19 -0700

    actually added pkg

---
+++ frontend/wasm/bezier/pkg/bezier_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_point_free: (a: number, b: number) => void;
+export const __wbg_get_point_x: (a: number) => number;
+export const __wbg_set_point_x: (a: number, b: number) => void;
+export const __wbg_get_point_y: (a: number) => number;
+export const __wbg_set_point_y: (a: number, b: number) => void;
+export const point_new: (a: number, b: number) => number;
+export const points_to_path: (a: number, b: number, c: number) => [number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
